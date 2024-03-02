5a5bf07af607e761ce03bb13b9dc16198d9c29d8
MihirKotecha <mihir.k1@ahduni.edu.in>
2024-03-02 13:29:23 +0530

    Link Restaurant Pages(Fix Done)

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,28 +1,36 @@
 import Shimmer from "./Shimmer";
 import { MENU_URL, CDN_URL } from "../utils/constants";
 import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import { useParams } from "react-router-dom"; 
 
 const RestaurantMenu = () => {
   useEffect(() => {
     fetchData();
   }, []);
 
-  const { redId } = useParams();
+  // const { redId } = useParams();
 
   const [menu, setMenu] = useState([]);
   const [resInfo, setResInfo] = useState([]);
-  const { urlId } = useParams();
-
+  const param = useParams();
+  console.log(param);
   const fetchData = async () => {
-    const response = await fetch(MENU_URL + urlId);
-    console.log(urlId);
+   try{ const response = await fetch(MENU_URL + param.id);
+    // console.log(urlId);
     const json = await response.json();
-    const info = json?.data?.cards[2]?.card?.card;
-    const menuItems = json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.categories[0]?.itemCards;
-    setResInfo(info);
-    setMenu(menuItems);
+    const info = json?.data?.cards[0]?.card?.card;
+    const menuItems =
+      json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
+        ?.card?.itemCards;
+
+    // console.log(info);
+    // console.log(menuItems);
+    setResInfo(info || []);
+    setMenu(menuItems || []);}
+    catch(err){
+      console.log("error in fetching data");
+    } 
   };
 
   if (resInfo.length === 0 || menu.length === 0) return <Shimmer />;
